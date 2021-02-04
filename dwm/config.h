b6b111f180b1ea458115709e7074e75229cbac5e
Kafva <Martensson.Jonas98@gmail.com>
2021-02-04 20:15:45 +0100

    -

--- dwm/config.h
+++ dwm/config.h
@@ -78,7 +78,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon,  NULL };
-static const char *termcmd[]  = { "hyper", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 static const char *screenCap[] = { "/home/jonas/linsc/screenshot.bash", NULL };
 static const char *emojiMenu[] = { "/home/jonas/linsc/emoji.bash", NULL };
